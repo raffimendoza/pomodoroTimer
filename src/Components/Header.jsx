0bf22876dd6c8945c2f826fbdc0c1c9e2b652d45
Raffi Mendoza <>
2022-05-11 12:22:32 -0400

    update

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -7,14 +7,14 @@ export function Header () {
         <div class="container1">
             <div class="container1image"><img src={logo} alt="ischool logo" width="100px"/></div>
             <div class="container1header">
-                IST 263 - M002 <br/>
-                Class References, Shortcuts, Tools
+                <h2> <p>IST 263 - M002</p>
+                <p> Class References, Shortcuts, Tools</p> </h2>
             </div>
         </div>
         <nav>
-            <div> <a href="https://raffimendoza.github.io/ist263/project7/schedule.html">SCHEDULE</a></div>
-            <div> <a href="https://raffimendoza.github.io/ist263/project7/index.html">HOME</a></div> 
-            <div> <a href="https://raffimendoza.github.io/pomodoroTimer/"><strong>POMODORO</strong></a></div>
+            <div> <a href="https://raffimendoza.github.io/ist263/project7/schedule.html" id="schedule">SCHEDULE</a></div>
+            <div> <a href="https://raffimendoza.github.io/ist263/project7/index.html" id="home">HOME</a></div> 
+            <div> <a href="https://raffimendoza.github.io/pomodoroTimer/" id="pomodoro"><strong>POMODORO</strong></a></div>
         </nav>
     </header>
   )
